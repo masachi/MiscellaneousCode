7bbba576d8bda482fb87dd8ddf10a426617db5d2
YukinoshitaKyaru <zhangjb42@gmail.com>
2025-06-10 22:22:41 +0800

    Update context.ts

--- ts/context.ts
+++ ts/context.ts
@@ -18,3 +18,41 @@ const defaultValue: GridItemContextItem = {};
 const GridItemContext = createContext(defaultValue);
 
 export default GridItemContext;
+
+
+
+
+import { useLocation } from 'umi';
+import { createContext, useContext } from 'react';
+
+export const useRouteProps = () => {
+  const routes = useRouteContext()?.routes ?? [];
+  const location = useLocation();
+  // use `useLocation` get location without `basename`, not need `basename` param
+  const currentRoute = routes
+    ?.filter((item) => item?.path === location.pathname)
+    ?.at(0);
+  return currentRoute?.params ?? {};
+};
+
+interface RouteContextItem {
+  routes?: any[];
+}
+
+const defaultValue: RouteContextItem = {};
+
+export const useRouteContext = () => useContext(RouteContext);
+
+export const RouteContext = createContext(defaultValue);
+
+export default (props: any) => {
+  return (
+    <RouteContext.Provider
+      value={{
+        routes: props?.routes,
+      }}
+    >
+      {props.children}
+    </RouteContext.Provider>
+  );
+};
